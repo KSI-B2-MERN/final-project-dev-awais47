daeae1522fa2320c7c7df163f4d86bd407d138e1
Muhammad Awais <muhammadawaisijaz010@gmail.com>
2023-10-15 14:55:32 +0500

    NewsApp completed

--- src/components/News.js
+++ src/components/News.js
@@ -1,39 +1,109 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { useEffect, useState } from 'react';
+import NewsItem from './NewsItem';
+import Spinner from './Spinner';
+import PropTypes from 'prop-types';
 
-export class News extends Component {
-  
-  constructor(){
-    super();
-    this.state = {
-      articles: []
-    }
-  }
-  
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/everything?q=tesla&from=2023-09-04&sortBy=publishedAt&apiKey=c0625a199e7445af92f969adf08c9c95";
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const updateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${props.category}&apiKey=c0625a199e7445af92f969adf08c9c95&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url);
-    let parseData = await data.json();
-    console.log(parseData);
-    this.setState({articles:parseData.articles});
-  }
+    props.setProgress(30);
+    let parsedData = await data.json();
+    props.setProgress(70);
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    props.setProgress(100);
+  };
 
-  render() {
-    return (
-      <div className="container my-3">
-        <h2>NewsApp - Top HeadLines</h2>
-        
-        <div className='row '>
-        {this.state.articles.map((element)=>{
-          return <div className='col md-3 my-3' key= {element.url}>
-            <NewsItem title={element.title.slice(0, 40)} description={element.description.slice(0, 80)} imageUrl={element.urlToImage} newsUrl={element.url} />
-          </div>
-        })}
-          
+  useEffect(() => {
+    document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
+    updateNews();
+    // eslint-disable-next-line
+  }, []);
+
+  const handlePrevClick = () => {
+    if (page > 1) {
+      setPage(page - 1);
+      updateNews();
+    }
+  };
+
+  const handleNextClick = () => {
+    if (page + 1 <= Math.ceil(totalResults / props.pageSize)) {
+      setPage(page + 1);
+      updateNews();
+    }
+  };
+
+  return (
+    <>
+      <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>
+        NewsApp - Top {capitalizeFirstLetter(props.category)} Headlines
+      </h1>
+      {loading && <Spinner />}
+      <div className="container">
+        <div className="row">
+          {articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ''}
+                  description={element.description ? element.description : ''}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
-    )
-  }
-}
+      <div className="container d-flex justify-content-between">
+        <button
+          disabled={page <= 1}
+          type="button"
+          className="btn btn-dark"
+          onClick={handlePrevClick}
+        >
+          &larr; Previous
+        </button>
+        <button
+          disabled={page + 1 > Math.ceil(totalResults / props.pageSize)}
+          type="button"
+          className="btn btn-dark"
+          onClick={handleNextClick}
+        >
+          Next &rarr;
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default News
+News.defaultProps = {
+  country: 'us',
+  pageSize: 8,
+  category: 'general',
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
+
+export default News;
